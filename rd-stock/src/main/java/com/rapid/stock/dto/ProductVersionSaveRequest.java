09b82a268f3a02661a44a46f2a094ccf3b66043d
Simon Vargas <c-simon.vargas@hulu.com>
2022-04-08 12:23:26 -0500

    allowing to save option categories and product availability objects as nulls

--- rd-stock/src/main/java/com/rapid/stock/dto/ProductVersionSaveRequest.java
+++ rd-stock/src/main/java/com/rapid/stock/dto/ProductVersionSaveRequest.java
@@ -18,7 +18,7 @@ public class ProductVersionSaveRequest {
     @Getter(AccessLevel.NONE)
     private boolean isAvailable;
     private List<String> optionCategoriesIds;
-    private List<ProductAvailabilityDTO> productAvailabilitiesDto;
+    private List<ProductAvailabilityDTO> productAvailabilities;
 
     @JsonProperty("isAvailable")
     public boolean isAvailable(){
--- rd-stock/src/main/java/com/rapid/stock/mapper/ParentProductMapper.java
+++ rd-stock/src/main/java/com/rapid/stock/mapper/ParentProductMapper.java
@@ -4,16 +4,12 @@ import com.rapid.stock.dto.ParentProductSaveRequest;
 import com.rapid.stock.dto.ProductAvailabilityDTO;
 import com.rapid.stock.dto.ProductVersionSaveRequest;
 import com.rapid.stock.model.*;
-import com.rapid.stock.repository.OptionCategoryRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -45,26 +41,32 @@ public class ParentProductMapper {
                                               .isAvailable(pvSaveRequest.isAvailable())
                                               .createdAt(LocalDateTime.now())
                                               .optionCategories( getOptionCategories(pvSaveRequest.getOptionCategoriesIds()) )
-                                              .productAvailabilities( mapToProductAvailabilityList(pvSaveRequest.getProductAvailabilitiesDto()) )
+                                              .productAvailabilities( mapToProductAvailabilityList(pvSaveRequest.getProductAvailabilities()) )
                                               .build();})
                                       .collect(Collectors.toList());
        }
 
        public List<ProductAvailability> mapToProductAvailabilityList(List<ProductAvailabilityDTO> productAvailabilityDTOList){
-              return productAvailabilityDTOList.stream()
-                                               .map(prodAvailDto ->
-                                                        ProductAvailability.builder()
-                                                        .companySiteID(prodAvailDto.getCompanySiteID())
-                                                        .quantityAvailable(prodAvailDto.getQuantityAvailable())
-                                                        .createdAt(LocalDateTime.now())
-                                                        .build())
-                                               .collect(Collectors.toList());
+              if(productAvailabilityDTOList != null && !productAvailabilityDTOList.isEmpty())
+                     return productAvailabilityDTOList.stream()
+                                                      .map(prodAvailDto ->
+                                                               ProductAvailability.builder()
+                                                               .companySiteID(prodAvailDto.getCompanySiteID())
+                                                               .quantityAvailable(prodAvailDto.getQuantityAvailable())
+                                                               .createdAt(LocalDateTime.now())
+                                                               .build())
+                                                      .collect(Collectors.toList());
+              else
+                     return null;
        }
 
 
        public List<OptionCategory> getOptionCategories(List<String> optionCategoryIds){
-              return optionCategoryIds.stream()
+              if(optionCategoryIds != null && !optionCategoryIds.isEmpty())
+                 return optionCategoryIds.stream()
                                       .map(optionCategoryId -> mongoTemplate.findById(optionCategoryId, OptionCategory.class))
                                       .collect(Collectors.toList());
+              else
+                   return null;
        }
 }
