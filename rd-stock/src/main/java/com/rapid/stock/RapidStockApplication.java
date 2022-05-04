5d2279aea89c05342646171c36788bd4d6e01a8a
Simon Vargas <c-simon.vargas@hulu.com>
2022-05-03 23:26:06 -0500

    removing unused packages from main class

--- rd-stock/src/main/java/com/rapid/stock/RapidStockApplication.java
+++ rd-stock/src/main/java/com/rapid/stock/RapidStockApplication.java
@@ -1,27 +1,11 @@
 package com.rapid.stock;
 
-import com.rapid.stock.model.*;
-import com.rapid.stock.repository.ParentProductRepository;
-import com.rapid.stock.repository.OptionCategoryRepository;
-import org.bson.types.ObjectId;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.core.MongoTemplate;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.UUID;
 
 @SpringBootApplication
 public class RapidStockApplication {
     public static void main(String[] args){
         SpringApplication.run(RapidStockApplication.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner runner(ParentProductRepository parentProdRepository, OptionCategoryRepository optionCategoryRepository, MongoTemplate mongoTemplate){
-           return args -> {};
     }
 }
