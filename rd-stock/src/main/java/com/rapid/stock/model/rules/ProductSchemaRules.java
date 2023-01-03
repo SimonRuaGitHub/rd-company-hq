f124ffddf5aa9bfa18fb0439a975dfc6e42ce295
SimonRuaGitHub <simonfruavargas@gmail.com>
2023-01-02 23:04:32 -0800

    Revert "feat: not creating repeated product name for same company"
    This reverts commit 04b79c40b026a8239f56616c06462ec284a576d3.

--- rd-stock/src/main/java/com/rapid/stock/model/rules/ProductSchemaRules.java
+++ rd-stock/src/main/java/com/rapid/stock/model/rules/ProductSchemaRules.java
@@ -1,8 +1,5 @@
 package com.rapid.stock.model.rules;
 
-import com.rapid.stock.exception.NotValidProductException;
-import com.rapid.stock.exception.NotValidRackException;
-import com.rapid.stock.repository.v2.ParentProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -10,12 +7,4 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class ProductSchemaRules {
 
-    private final ParentProductRepository productRepository;
-
-    public String noRepeatedProductNameForCompany(String name, String companyId){
-        if(productRepository.existsByNameAndCompanyId(name, companyId))
-            throw new NotValidProductException("There is already a product name of: "+name+" created for company id: "+companyId);
-        else
-            return name;
-    }
 }
