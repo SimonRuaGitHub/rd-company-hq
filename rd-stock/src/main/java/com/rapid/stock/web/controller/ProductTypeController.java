e282ca750fb448e16fe78c75dc7845618bae9c37
SimonRuaGitHub <simonfruavargas@gmail.com>
2023-01-03 18:27:22 -0800

    feat: adding post mapping http method for product type api

--- rd-stock/src/main/java/com/rapid/stock/web/controller/ProductTypeController.java
+++ rd-stock/src/main/java/com/rapid/stock/web/controller/ProductTypeController.java
@@ -5,6 +5,7 @@ import com.rapid.stock.service.ProductTypeService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ public class ProductTypeController {
 
     private final ProductTypeService productTypeService;
 
+    @PostMapping
     public ResponseEntity saveType(@RequestBody ProductTypeDTO productTypeDTO){
            productTypeService.save(productTypeDTO);
            return ResponseEntity.status(HttpStatus.CREATED).build();
