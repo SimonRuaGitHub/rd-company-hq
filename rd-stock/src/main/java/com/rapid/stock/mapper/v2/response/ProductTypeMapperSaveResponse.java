d5efc9d1a7d1c7474940d7942f3ecb13482fa540
SimonRuaGitHub <simonfruavargas@gmail.com>
2024-04-25 19:22:11 -0500

    Fixing mapper for product type

--- rd-stock/src/main/java/com/rapid/stock/mapper/v2/response/ProductTypeMapperSaveResponse.java
+++ rd-stock/src/main/java/com/rapid/stock/mapper/v2/response/ProductTypeMapperSaveResponse.java
@@ -2,7 +2,9 @@ package com.rapid.stock.mapper.v2.response;
 
 import com.rapid.stock.dto.ProductTypeSaveResponse;
 import com.rapid.stock.model.v2.ProductType;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ProductTypeMapperSaveResponse {
     public ProductTypeSaveResponse map(ProductType productType) {
         return  ProductTypeSaveResponse
