092848a4c876fd3195f2ed8d9ff8de8526b48bd2
Simon Rua <simonfruavargas@gmail.com>
2024-10-16 17:18:11 -0500

    Using internal credentials for AWS and dividing configuration beans

--- rd-stock/src/main/java/com/rapid/stock/config/AWSConfigLocal.java
+++ rd-stock/src/main/java/com/rapid/stock/config/AWSConfigLocal.java
@@ -1,30 +1,30 @@
 package com.rapid.stock.config;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.*;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 
 @Configuration
-public class StorageFilesConfig {
+@Profile({"local","local-docker"})
+public class AWSConfigLocal {
 
-    @Value("${cloud.aws.credentials.access-key}")
+    @Value("${cloud.aws.localstack.mock-credentials.access-key}")
     private String accessKey;
-    @Value("${cloud.aws.credentials.secret-key}")
+    @Value("${cloud.aws.localstack.mock-credentials.secret-key}")
     private String secretKey;
     @Value("${cloud.aws.region.static}")
     private String region;
     @Value("${cloud.aws.s3.url}")
-    private String url;
+    private String s3Url;
 
     @Bean
-    @Profile({"local","local-docker"})
     public AmazonS3 localS3Client() {
 
         return AmazonS3ClientBuilder.standard()
@@ -34,26 +34,14 @@ public class StorageFilesConfig {
                 .build();
     }
 
-    @Bean
-    @Profile("development")
-    public AmazonS3 devS3Client() {
+    private AWSCredentialsProvider getCredentialsProvider() {
 
-        return AmazonS3ClientBuilder.standard()
-                .withCredentials(getCredentialsProvider())
-                .withRegion(region)
-                .build();
-    }
-
-    private AWSStaticCredentialsProvider getCredentialsProvider() {
-        AWSCredentials credentials = new BasicAWSCredentials(
-                accessKey,
-                secretKey
-        );
+        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
 
         return new AWSStaticCredentialsProvider(credentials);
     }
 
     private AwsClientBuilder.EndpointConfiguration getLocalEndpointConfiguration() {
-        return new AwsClientBuilder.EndpointConfiguration(url, region);
+        return new AwsClientBuilder.EndpointConfiguration(s3Url, region);
     }
 }
--- rd-stock/src/main/java/com/rapid/stock/config/ObjectMapperTimeModuleConfig.java
+++ rd-stock/src/main/java/com/rapid/stock/config/ObjectMapperTimeModuleConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class Config {
+public class ObjectMapperTimeModuleConfig {
     @Bean
     public ObjectMapper mapperJson(){
         ObjectMapper objectMapper = new ObjectMapper();
